79dd6efbf226997a2a04f5871b2bdca9c28f27d1
Haq Nawaz <haqnawaz99@yahoo.com>
2021-08-20 21:14:56 +0500

    first one

---
+++ SuperHeroDB/Server/Controllers/SuperHeroController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperHeroDB.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+
+    }
+}
